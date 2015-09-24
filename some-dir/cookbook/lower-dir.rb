e0a63724cd15cbdde994804c8197809ed40b7aea
efueger <emily@fueger.net>
2015-09-24 12:06:10 -0400

    Update lower-dir.rb

--- some-dir/cookbook/lower-dir.rb
+++ some-dir/cookbook/lower-dir.rb
@@ -1,4 +1,4 @@
-class lower-dir < ActionController::Base
+class lowerdir < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
